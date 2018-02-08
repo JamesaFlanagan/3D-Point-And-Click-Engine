2a2d9b8a7e2b3ac7c8ae020c2ab54bbcb9b40c23
James A Flanagan <jamesaflanagan@ymail.com>
2018-02-08 12:59:38 +0000

    Can now generate a dynamic list of properties for an object to add

---
+++ 3DPointAndClick/Scripts/GameFileCreator.js
@@ -0,0 +1,28 @@
+﻿var createableObjects =
+    [
+        { objectType:"square", objectParams:[{ name:"x", type:"number" }, { name:"y", type:"number" }, { name:"w", type:"number" }, { name:"h", type:"number" }, { name:"color", type:"color" }] }
+    ];
+
+var generateCreateObjectParamList = function (div, objectType) {
+
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+
+    var objectToCreate = _.find(createableObjects, function (a) { return a.objectType == objectType; });
+
+
+    _.each(objectToCreate.objectParams, function (value, key, list) {
+
+        var newDiv = document.createElement('div');
+
+        var title = document.createElement('span');
+        title.innerText = value.name;
+
+        newDiv.appendChild(title);
+
+        div.appendChild(newDiv);
+        
+    });
+
+}
