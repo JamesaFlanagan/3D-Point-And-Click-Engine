039b104ea5d6712aacc4d82ab73569959d27b500
James A Flanagan <jamesaflanagan@ymail.com>
2018-02-09 17:50:34 +0000

    Can now create objects dynamically from the list of attributes

--- 3DPointAndClick/Scripts/GameFileCreator.js
+++ 3DPointAndClick/Scripts/GameFileCreator.js
@@ -11,6 +11,21 @@ var generateCreateObjectParamList = function (div, objectType) {
 
     var objectToCreate = _.find(createableObjects, function (a) { return a.objectType == objectType; });
 
+    {
+        var newDiv = document.createElement('div');
+
+        var title = document.createElement('span');
+        title.innerText = "name";
+
+        var input = document.createElement('input');
+        input.type = 'text'
+
+        newDiv.appendChild(title);
+        newDiv.appendChild(input);
+
+        div.appendChild(newDiv);
+    }
+
 
     _.each(objectToCreate.objectParams, function (value, key, list) {
 
@@ -19,10 +34,29 @@ var generateCreateObjectParamList = function (div, objectType) {
         var title = document.createElement('span');
         title.innerText = value.name;
 
+        var input = document.createElement('input');
+        input.type = 'text'
+
         newDiv.appendChild(title);
+        newDiv.appendChild(input);
 
         div.appendChild(newDiv);
         
     });
 
+}
+
+var getObjectDataFromParamListDiv = function (div, type) {
+
+    var object = {};
+
+    object.type = type;
+
+    _.forEach(div.children, function (value, key, list) {
+        
+        object[value.children[0].innerText] = value.children[1].value;
+        
+    });
+
+    return object;
 }
