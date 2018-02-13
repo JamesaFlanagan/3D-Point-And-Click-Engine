2980d7813aa30c88fff54cffcadf8ab7ae288cf4
James A Flanagan <jamesaflanagan@ymail.com>
2018-02-13 12:35:37 +0000

    Can now load the values of an object to the screen

--- 3DPointAndClick/Scripts/GameFileCreator_Logic.js
+++ 3DPointAndClick/Scripts/GameFileCreator_Logic.js
@@ -2,7 +2,10 @@
 var Canvas = null;
 var CreateableTypesList = null;
 var CurrentObjectList = null;
+var CurrentObjectDiv = null;
 
+var currentObjectCount = 0; //TODO - Fix this won't work when a file is loaded in.
+//TODO - need to enforce unique names - so find works properly  
 
 var reloadGame = function () {
     DisplayText.innerText = "";
@@ -15,7 +18,7 @@ var setupCreatorScreen = function () {
     Canvas = document.getElementById("Canvas");
     CreateableTypesList = document.getElementById("CreateableTypes");
     CurrentObjectList = document.getElementById("CurrentObjectList");
-    
+    CurrentObjectDiv = document.getElementById("CurrentObject");   
 
 
     var createableObjectTypes = CreateableTypesList;
@@ -31,6 +34,9 @@ var setupCreatorScreen = function () {
     });
 
 }
+
+
+
 
 var generateCreateObjectParamList = function (div, objectType) {
 
@@ -90,6 +96,55 @@ var getObjectDataFromParamListDiv = function (div, type) {
     return object;
 }
 
+var showEditObjectList = function (itemDiv) {
+
+    while (CurrentObjectDiv.firstChild) {
+        CurrentObjectDiv.removeChild(CurrentObjectDiv.firstChild);
+    }
+
+    var objectName = itemDiv.innerText;
+
+    var scene = _.find(currentGameFile.scenes, function (value) { return value.name = currentScene.name; });
+    var object = _.find(scene.objects, function (value) { return value.name = objectName; });
+    var objectType = _.find(createableObjects, function (value) { return value.objectType = object.type; });
+    
+    {
+        var newDiv = document.createElement('div');
+
+        var title = document.createElement('span');
+        title.innerText = "name";
+
+        var input = document.createElement('input');
+        input.type = 'text'
+        input.value = objectName;
+
+        newDiv.appendChild(title);
+        newDiv.appendChild(input);
+
+        CurrentObjectDiv.appendChild(newDiv);
+    }
+
+
+    _.each(objectType.objectParams, function (value, key, list) {
+
+        var newDiv = document.createElement('div');
+
+        var title = document.createElement('span');
+        title.innerText = value.name;
+
+        var input = document.createElement('input');
+        input.type = 'text'
+        input.value = object[value.name];
+
+        newDiv.appendChild(title);
+        newDiv.appendChild(input);
+
+        CurrentObjectDiv.appendChild(newDiv);
+
+    });
+
+}
+
 var refreshUIValues = function () {
 
     var scene = _.find(currentGameFile.scenes, function (value) { return value.name = currentScene.name; });
@@ -102,6 +157,8 @@ var refreshUIValues = function () {
 
         var newDiv = document.createElement('div');
         newDiv.innerText = value.name; //TODO - will need to give them a default name of object number etc
+
+        newDiv.onclick = function () { showEditObjectList(newDiv); };
 
         CurrentObjectList.appendChild(newDiv);
     });
